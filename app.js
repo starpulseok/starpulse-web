197908a4d4c10bb605911bd9a8a123e2671440ac
starpulseok <starpulse.contacto@gmail.com>
2026-01-23 13:19:34 -0300

    Mejorar mensaje de WhatsApp

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const phone = "5493564661474";
-const msg = "Hola Star Pulse! Quiero consultar por un armado o producto.";
+const msg = "Hola Star Pulse! Quiero cotizar un armado o consultar productos. Mi presupuesto es ... y lo necesito para ...";
 
 const link = `https://wa.me/${phone}?text=${encodeURIComponent(msg)}`;
 
